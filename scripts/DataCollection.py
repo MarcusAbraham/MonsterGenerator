16504528804348b2005f6842c08543685555ea83
Marcus A <marcus.abraham100@gmail.com>
2023-04-19 05:37:38 +0100

    Starting to extract monster descriptions

--- scripts/DataCollection.py
+++ scripts/DataCollection.py
@@ -1,7 +1,4 @@
-import re
 from pdfminer.high_level import extract_text
-import string
-import csv
 
 
 def gather_index():
@@ -16,15 +13,30 @@ def gather_index():
     return monster_dict
 
 
-def gather_monsters():
+def gather_monsters(index_dict):
     # Extract pdf and split into pages
     pdf_text = extract_text('../resources/MonsterManual.pdf')
     pages = pdf_text.split('\f')
 
-    print(pages[32])
+    for monster_name, page_number in index_dict.items():
+        # Check up to 10 pages before and after the monster's statblock page
+        for page in range(page_number - 10, page_number + 10):
+            if page < 0 or page >= len(pages):
+                continue
+
+            page_text = pages[page]
+            # Look for headings with the same name in all capital letters
+            if monster_name.upper() in page_text:
+                start_index = page_text.find(monster_name.upper())
+                end_index = page_text.find('\n\n', start_index)
+
+                # Copy all text under the heading until the next capitalised heading
+                monster_description = page_text[start_index:end_index].strip()
+                print(f"{monster_name}: {monster_description}")
+                break
 
 
 index_dict = gather_index()
 print(index_dict)
 
-gather_monsters()
+gather_monsters(index_dict)
